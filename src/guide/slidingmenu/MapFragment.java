eff02d6595869ea4afe19730646ee93f8a3e9ae0
GeRomeCarvajal <geromecarvajal@gmail.com>
2014-09-28 23:33:21 +0800

    map
    sir ayw talaga sa  .getMap();

--- src/guide/slidingmenu/MapFragment.java
+++ src/guide/slidingmenu/MapFragment.java
@@ -1,56 +1,45 @@
+
 package guide.slidingmenu;
 
 import android.app.Fragment;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import edu.ucuccs.ucufreshmenguide.R;
 
 public class MapFragment extends Fragment {
 	GoogleMap map;
-	public MapFragment(){}
-	 static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-     static final LatLng KIEL = new LatLng(53.551, 9.993);
-     
+	private static View view;
+	 
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+
+
+		view = inflater.inflate(R.layout.fragment_map, container,
+				false);
+		map = ((MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+
 		
-		
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+      //  View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+      //  getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         
         
-    //    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+    // map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-            Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                    .fromResource(R.drawable.ic_launcher)));
 
-            // Move the camera instantly to hamburg with a zoom of 15.
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-            // Zoom in, animating the camera.
-            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
         
-        return rootView;
+        return view;
         
        
     }
+	
+	
+	
 }
+
